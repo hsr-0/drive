b73a0e5345e5a114c7c799ac9f00f0dfb2d5ae71
calin9p <calin9p@gmail.com>
2026-01-28 21:53:13 +0300

    تحديث

--- lib/environment.dart
+++ lib/environment.dart
@@ -1,7 +1,7 @@
 class Environment {
   // ATTENTION Please update your desired data.
   static const String appName = 'OvoRide Driver';
-  static const String version = '1.0.0';
+  static const String version = '1.0.1';
 
   // Ride and Bids
   static const int bidAcceptSecond = 30; //Bid ACCEPT second
